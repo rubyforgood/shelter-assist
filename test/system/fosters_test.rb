e1d43ceaf33eb7e4d79cc934924a6641a599c777
Chris Zimmerman <chris@chriszimmerman.net>
2021-09-24 16:05:50 -0400

    Add form input asking for home availability during the day.

--- test/system/fosters_test.rb
+++ test/system/fosters_test.rb
@@ -8,6 +8,10 @@ class FostersTest < ApplicationSystemTestCase
     assert_selector "label", text: "Email"
 
     fill_in "Email", with: "peter@github.com"
+
+    assert_selector "h2", text: "HOME INFORMATION"
+    assert_selector "label", text: "Are you or another adult home during the day?"
+
     click_on "Fetch!"
 
     assert_selector "h1", text: "Woof."
