e05b59227810f44cda530ef69309d3304f399506
Jim Gay <jim@saturnflyer.com>
2021-09-25 12:09:24 -0400

    Fix routes to ensure ours comes before devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :animal_applications
   resources :homes
+  resources :fosters
   devise_for :fosters
   passwordless_for :fosters, at: '/', as: :auth
 
   post 'signups', to: 'signups#create', as: :signup
   get 'signups', to: 'signups#new', as: :new_signup
-  resources :fosters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
