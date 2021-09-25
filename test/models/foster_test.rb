6b64a67135d00f560814b035f6f3dcbb06606911
Peter Cho <pcho231@gmail.com>
2021-09-25 10:25:28 -0400

    Consolidated mock data for tests (as list of fields grow)

--- test/models/foster_test.rb
+++ test/models/foster_test.rb
@@ -1,37 +1,55 @@
 require "test_helper"
 
 class FosterTest < ActiveSupport::TestCase
-  test 'valid foster' do
-    foster = Foster.new(email: 'johndoe@mail.com', phone: '4017824567', street: '123', is_home_during_day: true)
+  def setup
+    @valid_parameters = {
+      email: 'johndoe@mail.com',
+      phone: '4017824567',
+      street: '123 Coconut Ave',
+      is_home_during_day: true
+    }
+  end
+
+  test 'inserts a valid foster into the fosters table' do
+    foster = Foster.new(@valid_parameters)
+
     assert foster.valid?
   end
 
-  test 'does not add a foster with an empty street' do
-    foster = Foster.new(email: 'johncena@mail.com', phone: '4017824567', street: '', is_home_during_day: true)
+  test 'a foster can be rejected for an empty street' do
+    bad_parameters = @valid_parameters
+    bad_parameters['street'] = ''
+    foster = Foster.new(bad_parameters)
+
     assert foster.invalid?
   end
 
   test 'does add a foster without apt' do
-    foster = Foster.new(email: 'johnwick@mail.com', phone: '4017824567', street: '987 street', apt: '', is_home_during_day: true)
+    valid_parameters = @valid_parameters
+    valid_parameters['apt'] = ''
+    foster = Foster.new(valid_parameters)
+
     assert foster.valid?
   end
 
-  test 'does add a foster with apt' do
-    foster = Foster.new(email: 'johnwick@mail.com', phone: '4017824567', street: '987 street', apt: '#123', is_home_during_day: true)
-    assert foster.valid?
-  end
-  
-  test "a foster can be added with valid entries" do
-    foster = Foster.create(email: 'chuck.norris@jetli.com', phone: '4017824567', street: '123', is_home_during_day: true)
+  test 'does add a foster with an apt' do
+    valid_parameters = @valid_parameters
+    valid_parameters['apt'] = 'Suite #789'
+    foster = Foster.new(valid_parameters)
 
     assert foster.valid?
   end
 
   test "a foster can be rejected for bad entries" do
-    bad_foster_1 = Foster.create(email: 'chuck.norris@jetli.com', phone: '1234567890', street: '123')
-    bad_foster_2 = Foster.create(email: 'chuck.norris@jetli.com', phone: '401-782-4567', street: '123')
-    bad_foster_3 = Foster.create(email: 'chuck.norris@jetli.com', phone: '4017824567asdf', street: '123')
-    bad_foster_4 = Foster.create(email: 'chuck.norris@jetli.com', phone: 'ruby for bad', street: '123')
+    bad_parameters = @valid_parameters
+    bad_parameters['phone'] = '1234567890'
+    bad_foster_1 = Foster.new(bad_parameters)
+    bad_parameters['phone'] = '401-789-4567'
+    bad_foster_2 = Foster.new(bad_parameters)
+    bad_parameters['phone'] = '401-789-4567asdf'
+    bad_foster_3 = Foster.new(bad_parameters)
+    bad_parameters['phone'] = 'ruby for bad'
+    bad_foster_4 = Foster.new(bad_parameters)
 
     assert bad_foster_1.invalid?
     assert bad_foster_2.invalid?
