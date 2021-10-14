40de3a5767a936d9588aebc9395651a7203ff1d2
Idan Zonshein <idan.zonshein@powerhrg.com>
2021-10-14 11:18:33 -0400

    Remove unused controller crud methods

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,23 +1,5 @@
 class AnimalsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
     @animal = Animal.new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
