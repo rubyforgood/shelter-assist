7176c8f61b7d49818928d29d2249372f57ce5a98
Katherine Muedas <muedasnka@gmail.com>
2021-10-14 15:19:23 -0400

    Starts adding pet form styles and form fields

--- app/javascript/components/animals/AnimalForm.tsx
+++ app/javascript/components/animals/AnimalForm.tsx
@@ -1,85 +1,165 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
 
-import styles from './styles.module.css'
+const { Option } = Select;
 
-const formTarget = document.getElementById('animal-form')
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Divider,
+  Checkbox,
+  Radio,
+  Cascader,
+  DatePicker,
+  Row,
+  Typography,
+  Select,
+} from "antd"
 
-const FORM_ERRORS = window['signupFormErrors']
-window['signupFormErrors'] = undefined
+const { Title, Paragraph } = Typography
 
-function getForm() {
-  return fetch('/signup.json')
-    .then((response: any) => response.json())
-}
+const formTarget = document.getElementById("animal-form")
 
-const TextInput = ({value, label, name}) => (
+const FORM_ERRORS = window["signupFormErrors"]
+window["signupFormErrors"] = undefined
+
+const TextInput = ({ value, label, name }) => (
   <label>
     <input type="text" name={name} placeholder={label} value={value} />
   </label>
 )
 
-const Radio = ({checked = false, value, label, name}) => (
-  <label>
-    {label}
-    <input type="radio" value={value} name={name} />
-  </label>
-)
-
 const AnimalForm = () => {
   const [formData, setFormData] = useState()
-  if (!formData) {
-    getForm()
-      .then((data) => setFormData({...data, errors: FORM_ERRORS}))
-  }
+  const handleChange = (value) => (console.log(`selected ${value}`))
+
+  useEffect(() => {
+    fetch("/signup.json")
+      .then((response) => response.json())
+      .then((data) => setFormData({ ...data, errors: FORM_ERRORS }))
+  }, [])
 
   return (
     <div>
-        { formData && (
-        <div>
-          <h1>Sign In</h1>
-          <form
-              action={`${formData.path}`}
-              acceptCharset="UTF-8"
-              className={styles.form}
-              method="post"
-              >
-                <input type="hidden" name="authenticity_token" value={formData.token} />
-                <TextInput value={formData.full_name} name="person[full_name]" label="Full Name:" />
-                <TextInput value={formData.nick_name} name="person[nick_name]" label="Nickname:" />
-                <TextInput value={formData.email} name="person[email]" label="Email:" />
-                <TextInput value={formData.street} name="person[street]" label="Street:" />
-                <TextInput value={formData.apt} name="person[apt]" label="Apartment:" />
-                <TextInput value={formData.phone} name="person[phone]" label="Phone Number:" />
+      {formData && (
+        <Row>
+          <Col span={12} offset={6} >
+            <Title>Add a Pet</Title>
+            <Paragraph>
+              This will allow you to create a profile for a pet needing a Foster home.
+            </Paragraph>
+          </Col>
+          <Col span={12} offset={6}>
+            <Form
+              name="person"
+              labelCol={{ span: 6 }}
+              wrapperCol={{ span: 10 }}
+              initialValues={{ remember: true }}
+              // onFinish={onFinish}
+              autoComplete="off"
+            >
+              <input
+                type="hidden"
+                name="authenticity_token"
+                value={formData.token}
+              />
+              <Divider>Basic Info</Divider>
+              <Row>
+                <Col span={12}>
+                  <Form.Item
+                    label="Pet Type"
+                    name="animal_kind"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please select a pet type!"
+                      }
+                    ]}
+                  >
+                    <Select
+                        style={{ width: 120 }}
+                        onChange={handleChange}
+                        placeholder="Please Select"
+                    >
+                      <Option value="dog">Dog</Option>
+                      <Option value="cat">Cat</Option>
+                    </Select>
 
-                <h2>Home Information</h2>
-                <p>Are you or another adult home during the day?</p>
-                <fieldset>
-                  <Radio value={true} label="Yes" name="person[is_home_during_day]" />
-                  <Radio value={false} label="No" name="person[is_home_during_day]" />
-                </fieldset>
+                  </Form.Item>
+                  <Form.Item
+                    label="Pet Name"
+                    name="name"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the pet name!"
+                      }
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item
+                      name="sterilized"
+                      label="Sterilized"
+                  >
+                    <Radio.Group>
+                      <Radio value={true}>Yes</Radio>
+                      <Radio value={false}>No</Radio>
+                    </Radio.Group>
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                      name="gender"
+                      label="Gender"
+                  >
+                    <Radio.Group>
+                      <Radio value="male">Male</Radio>
+                      <Radio value="female">Female</Radio>
+                    </Radio.Group>
+                  </Form.Item>
+                  <Form.Item
+                    label="Breed"
+                    name="breed"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the pet's breed!"
+                      }
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
 
+                  <Form.Item
+                    label="Weight"
+                    name="weight"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the pet's weight!"
+                      }
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
 
-                <h2>Home Type</h2>
-                <select name="person[homes_attributes][0][home_type]">
-                  <option value="Apartment/Condo">Apartment/Condo</option>
-                  <option value="Townhouse">Townhouse</option>
-                  <option value="House">House</option>
-                </select>
-
-
-                <h2>Application</h2>
-                <textarea name="person[animal_application][inspiration]"></textarea>
-
-                <h2>Transportation</h2>
-                <select name="person[transportation]">
-                  <option value="access_to_car">Access to car</option>
-                  <option value="car">Car</option>
-                  <option value="no_car">No car</option>
-                </select>
-              <input type="submit" value="Fetch!" />
-          </form>
-        </div>
+                  <Form.Item
+                      name="microchiped"
+                      label="Microchiped"
+                  >
+                    <Radio.Group>
+                      <Radio value={true}>Yes</Radio>
+                      <Radio value={false}>No</Radio>
+                    </Radio.Group>
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
       )}
     </div>
   )
