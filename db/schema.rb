12ee0a01efa1500030c501771aa6a3e09ae02076
Idan Zonshein <idan.zonshein@powerhrg.com>
2021-10-14 10:41:18 -0400

    Add missing animal fields to animal record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_151254) do
+ActiveRecord::Schema.define(version: 2021_10_14_142021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(version: 2021_10_13_151254) do
     t.bigint "rescue_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "breed"
+    t.float "weight"
+    t.boolean "sterilized", default: false
+    t.boolean "microchiped", default: false
     t.index ["person_id"], name: "index_animals_on_person_id"
     t.index ["rescue_id"], name: "index_animals_on_rescue_id"
   end
@@ -88,14 +92,14 @@ ActiveRecord::Schema.define(version: 2021_10_13_151254) do
   create_table "passwordless_sessions", force: :cascade do |t|
     t.string "authenticatable_type"
     t.bigint "authenticatable_id"
-    t.datetime "timeout_at", precision: 6, null: false
-    t.datetime "expires_at", precision: 6, null: false
-    t.datetime "claimed_at", precision: 6
+    t.datetime "timeout_at", null: false
+    t.datetime "expires_at", null: false
+    t.datetime "claimed_at"
     t.text "user_agent", null: false
     t.string "remote_addr", null: false
     t.string "token", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["authenticatable_type", "authenticatable_id"], name: "authenticatable"
   end
 
