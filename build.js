b56c046beb61af67b4b3f6ec5a45798a4bd43b51
Stephen Marshall <smarshall1980@gmail.com>
2021-10-14 08:26:12 -0500

    Update build.js watcher

--- build.js
+++ build.js
@@ -2,6 +2,8 @@
 const fg = require('fast-glob'), path = require('path'), fs = require('fs');
 const cssModulesPlugin = require("esbuild-css-modules-plugin");
 
+const env = process.env.RAILS_ENV
+
 const COMPONENTS_SRC_DIR = path.resolve(__dirname, 'app/javascript/components'),
       COMPONENTS_OUTPUT_DIR = path.resolve(__dirname, 'app/assets/builds/components');
 
@@ -30,7 +32,7 @@ components.forEach((c, i) => {
       plugins: [
         cssModulesPlugin()
       ],
-      watch: true,
+      watch: env !== 'production',
     })
     .catch((error) => {
       console.log('*** Failed to build "' + out + '"')
