95b90af48dd38bdcca20c8d9e8e638b2fa752eea
Ayako Kurushima <ayako.kurushima@powerhrg.com>
2021-10-15 11:57:03 -0400

    Rename component, fix test

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,23 +1,15 @@
 class StatusController < PasswordlessController
   before_action :require_person!
   def show
-    @person = Person.includes(
-      :homes,
-      :animal_age_preferences,
-      :animal_gender_preferences,
-      :animal_kind_preferences,
-      :animal_size_preferences
-    ).find_by(id: current_person.id)
-
     respond_to do |format|
       format.html {}
       format.json { render json: {
-        person: @person,
-        homes: @person.homes,
-        animal_age_preferences: @person.animal_age_preferences,
-        animal_gender_preferences: @person.animal_gender_preferences,
-        animal_kind_preferences: @person.animal_kind_preferences,
-        animal_size_preferences: @person.animal_size_preferences,
+        person: current_person,
+        homes: current_person.homes,
+        animal_age_preferences: current_person.animal_age_preferences,
+        animal_gender_preferences: current_person.animal_gender_preferences,
+        animal_kind_preferences: current_person.animal_kind_preferences,
+        animal_size_preferences: current_person.animal_size_preferences,
       } }
     end
   end
--- app/javascript/components/statuses/PersonProfile.tsx
+++ app/javascript/components/statuses/PersonProfile.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 
-const pageTarget = document.getElementById("foster-profile")
+const pageTarget = document.getElementById("person-profile")
 
-const Show = () => {
+const PersonProfile = () => {
   const [personData, setPersonData] = useState({})
 
   const readableBoolean = (val: boolean) => {
@@ -26,33 +26,33 @@ const Show = () => {
           <p>Is home during the day: {readableBoolean(personData.person.is_home_during_day)}</p>
           <h1>Homes:</h1>
           { personData.homes.map((home: any) => (
-            <>
-              <p>{home.street}</p>
-              <p>{home.apt}</p>
-              <p>{`${home.city}, ${home.state} ${home.zip_code}`}</p>
-              <p>Home type: {home.home_type}</p>
-              <p>Has children: {readableBoolean(home.has_children)}</p>
-              <p>Has fenced yard: {readableBoolean(home.has_fenced_yard)}</p>
-              <p>Has other adult(s): {readableBoolean(home.has_other_adults)}</p>
-              <p>Has other dog(s): {readableBoolean(home.has_other_dog)}</p>
-              <p>Has other cat(s): {readableBoolean(home.has_other_cat)}</p>
-            </>
+            <div key={`home-${home.id}`}>
+              <p key={`street-${home.id}`}>{home.street}</p>
+              <p key={`apt-${home.id}`}>{home.apt}</p>
+              <p key={`csz-${home.id}`}>{`${home.city}, ${home.state} ${home.zip_code}`}</p>
+              <p key={`type-${home.id}`}>Home type: {home.home_type}</p>
+              <p key={`children-${home.id}`}>Has children: {readableBoolean(home.has_children)}</p>
+              <p key={`fence-${home.id}`}>Has fenced yard: {readableBoolean(home.has_fenced_yard)}</p>
+              <p key={`adults-${home.id}`}>Has other adult(s): {readableBoolean(home.has_other_adults)}</p>
+              <p key={`dog-${home.id}`}>Has other dog(s): {readableBoolean(home.has_other_dog)}</p>
+              <p key={`cat-${home.id}`}>Has other cat(s): {readableBoolean(home.has_other_cat)}</p>
+            </div>
           ))}
           <h1>Animal Kind Preferences</h1>
           { personData.animal_kind_preferences.map((animal_kind: any) => (
-            <p>{animal_kind.animal_value}</p>
+            <p key={`animal-kind-${animal_kind.id}`}>{animal_kind.animal_value}</p>
           ))}
           <h1>Animal Gender Preferences</h1>
           { personData.animal_gender_preferences.map((animal_gender: any) => (
-            <p>{animal_gender.animal_value}</p>
+            <p key={`animal-gender-${animal_gender.id}`}>{animal_gender.animal_value}</p>
           ))}
           <h1>Animal Size Preferences</h1>
           { personData.animal_size_preferences.map((animal_size: any) => (
-            <p>{animal_size.animal_value}</p>
+            <p key={`animal-size-${animal_size.id}`}>{animal_size.animal_value}</p>
           ))}
           <h1>Animal Age Preferences</h1>
           { personData.animal_age_preferences.map((animal_age: any) => (
-            <p>{animal_age.animal_value}</p>
+            <p key={`animal-age-${animal_age.id}`}>{animal_age.animal_value}</p>
           ))}
         </>
       )}
@@ -60,4 +60,4 @@ const Show = () => {
   )
 }
 
-ReactDOM.render(<Show/>, pageTarget)
+ReactDOM.render(<PersonProfile/>, pageTarget)
