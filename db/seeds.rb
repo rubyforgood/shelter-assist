547938dd948ffe1c6406791cb9bdc523090e9012
Katherine Muedas <muedasnka@gmail.com>
2021-10-15 11:25:21 -0400

    Updates animals table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-admin = Person.create!(
-  full_name: "Ad Ministrator",
-  nick_name: "Admin",
-  email: 'admin@test.com',
-  password: 'password',
-  street: "123 main st.",
-  is_home_during_day: true,
-  phone: '123-555-1212',
-  transportation: 'car'
-)
+# admin = Person.create!(
+#   full_name: "Ad Ministrator",
+#   nick_name: "Admin",
+#   email: 'admin@test.com',
+#   password: 'password',
+#   street: "123 main st.",
+#   is_home_during_day: true,
+#   phone: '123-555-1212',
+#   transportation: 'car'
+# )
 
-admin.add_role(:admin)
+# admin.add_role(:admin)
 
-person = Person.create!(
-  full_name: "Person",
-  email: "foster@test.com",
-  street: "123 main st.",
-  is_home_during_day: true,
-  phone: '123-555-1212',
-  transportation: 'car'
-)
+# person = Person.create!(
+#   full_name: "Person",
+#   email: "foster@test.com",
+#   street: "123 main st.",
+#   is_home_during_day: true,
+#   phone: '123-555-1212',
+#   transportation: 'car'
+# )
 
 #   Rescue.create!(
 #   file_name: "cutepic.png",
@@ -26,3 +26,26 @@ person = Person.create!(
 #   name: "Jon smith",
 #   phone_number: "123-456-7892"
 # )
+
+  Animal.create!(
+  name: "Canoli",
+  animal_kind: "dog",
+  gender: "female",
+  breed: "Akita",
+  birthdate: Date.new(2019, 5, 12)
+)
+Animal.create!(
+  name: "Rafo",
+  animal_kind: "dog",
+  gender: "male",
+  breed: "Beagle",
+  birthdate: Date.new(2020, 7, 16)
+
+)
+Animal.create!(
+  name: "Nalo",
+  animal_kind: "cat",
+  gender: "male",
+  breed: "Burmese",
+  birthdate: Date.new(2018, 6, 2)
+)
