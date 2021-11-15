cd8111bbae07354c91468ec09f40b642de202560
Stephen Marshall <smarshall1980@gmail.com>
2021-11-15 09:03:07 -0600

    Additional Tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'landing': '#13C2C2',
+      }
+    }
+  }
 }
